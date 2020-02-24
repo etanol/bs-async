c9bcff70341495aac89a711421cea3ce6d3b2c84
Isaac Jurado <diptongo@gmail.com>
2020-02-25 00:25:31 +0100

    Various improvements to the helper JsPromise module.
    Fix the catch/recover types so "try" exception cases are forced to return the
    same type as the body; like bind_catch and map_recover do.
    
    Minimize the amount of raw Javascript required to wrap plain values into an
    exception type.  And introduce the new exception type that holds a tagged
    Javascript value.
    
    Pending testing about the feasibility of using Caml_js_exceptions.Error vs
    Js.Exn.Error.

--- src/jsPromise.ml
+++ src/jsPromise.ml
@@ -1,30 +1,27 @@
 external map : ('a -> 'b [@bs.uncurry]) -> 'b Js.Promise.t = "then" [@@bs.send.pipe: 'a Js.Promise.t]
 external bind : ('a -> 'b Js.Promise.t [@bs.uncurry]) -> 'b Js.Promise.t = "then" [@@bs.send.pipe: 'a Js.Promise.t]
-external recover' : (Js.Promise.error -> 'b [@bs.uncurry]) -> 'b Js.Promise.t = "catch" [@@bs.send.pipe: 'a Js.Promise.t]
-external catch' : (Js.Promise.error -> 'b Js.Promise.t [@bs.uncurry]) -> 'b Js.Promise.t = "catch" [@@bs.send.pipe: 'a Js.Promise.t]
+external recover' : (Js.Promise.error -> 'a [@bs.uncurry]) -> 'a Js.Promise.t = "catch" [@@bs.send.pipe: 'a Js.Promise.t]
+external catch' : (Js.Promise.error -> 'a Js.Promise.t [@bs.uncurry]) -> 'a Js.Promise.t = "catch" [@@bs.send.pipe: 'a Js.Promise.t]
 external map_recover' : ('a -> 'b [@bs.uncurry]) -> (Js.Promise.error -> 'b [@bs.uncurry]) -> 'b Js.Promise.t = "then" [@@bs.send.pipe: 'a Js.Promise.t]
 external bind_catch' : ('a -> 'b Js.Promise.t [@bs.uncurry]) -> (Js.Promise.error -> 'b Js.Promise.t [@bs.uncurry]) -> 'b Js.Promise.t = "then" [@@bs.send.pipe: 'a Js.Promise.t]
 
 (* let is_caml_exception = Js.Exn.isCamlExceptionOrOpenVariant
  * let error_exn = Js.Exn.Error *)
 
-let wrap_in_error : 'a -> Caml_js_exceptions.t = [%raw {|
+exception JSValue of Js.Types.tagged_t
+let is_exception : 'a -> bool = [%raw {|
   function (value) {
-    if (value instanceof Error) {
-      return value;
-    } else if (typeof(value) === 'object') {
-      return new Error(JSON.stringify(value));
-    } else {
-      return new Error(value.toString());
-    }
+    return value instanceof Error;
   }
 |}]
 
 let error_to_exn any =
   if Caml_exceptions.caml_is_extension any then
     (Obj.magic any : exn)
+  else if is_exception any then
+    Caml_js_exceptions.Error (Obj.magic any : Caml_js_exceptions.t)
   else
-    Caml_js_exceptions.Error (wrap_in_error any)
+    JSValue (Js.Types.classify any)
 
 let recover handler promise =
   recover' (fun e -> error_to_exn e |> handler) promise
